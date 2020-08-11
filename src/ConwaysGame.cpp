800fa92ade158e29d16c3743e8584f4a6e605932
Shane McDonough <shanemcdman@gmail.com>
2020-08-11 15:55:38 -0700

    Change format of draw_grid_lines

--- src/ConwaysGame.cpp
+++ src/ConwaysGame.cpp
@@ -17,12 +17,10 @@ void ConwaysGame::create_grid(){
 }
 
 void ConwaysGame::draw_grid_lines(){
-    for(int i = 0; i <= window_size.y; i += scale){
+    for(int i = 0; i <= window_size.y; i += scale)
         DrawLine(0, i, window_size.x, i, Color{125, 125, 125, 125});
-    }
-    for(int i = 0; i <= window_size.x; i += scale){
+    for(int i = 0; i <= window_size.x; i += scale)
         DrawLine(i, 0, i, window_size.y, Color{125, 125, 125, 125});
-    }
 }
 
 void ConwaysGame::draw_grid(){
