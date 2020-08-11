74f02e9e4e2651b9a8f16baf5f35307ce5600a06
Shane McDonough <shanemcdman@gmail.com>
2020-08-11 15:06:13 -0700

    Add fps variable and another constructor

--- include/ConwaysGame.h
+++ include/ConwaysGame.h
@@ -5,13 +5,15 @@
 
 class ConwaysGame{
 private:
-public:
     Vector2 window_size;
     float scale;
+    int fps;
+public:
     ConwaysGame();
     ConwaysGame(float scl);
     ConwaysGame(float x, float y);
     ConwaysGame(float x, float y, float scl);
+    ConwaysGame(float x, float y, float scl, int f);
     void run();
 };
 
--- src/ConwaysGame.cpp
+++ src/ConwaysGame.cpp
@@ -1,18 +1,22 @@
 #include "ConwaysGame.h"
 
-ConwaysGame::ConwaysGame():ConwaysGame(900, 600, 10){
+ConwaysGame::ConwaysGame():ConwaysGame(900, 600, 10, 30){
 }
 
-ConwaysGame::ConwaysGame(float scl):ConwaysGame(900, 600, scl){
+ConwaysGame::ConwaysGame(float scl):ConwaysGame(900, 600, scl, 30){
 }
 
-ConwaysGame::ConwaysGame(float x, float y):ConwaysGame(x, y, 10){
+ConwaysGame::ConwaysGame(float x, float y):ConwaysGame(x, y, 10, 30){
 }
 
-ConwaysGame::ConwaysGame(float x, float y, float scl){
+ConwaysGame::ConwaysGame(float x, float y, float scl):ConwaysGame(x, y, scl, 30){
+}
+
+ConwaysGame::ConwaysGame(float x, float y, float scl, int f){
     window_size.x = x;
     window_size.y = y;
     scale = scl;
+    fps = f;
 }
 
 void ConwaysGame::run(){
