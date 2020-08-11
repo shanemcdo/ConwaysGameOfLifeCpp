ab14139f443e556e9658747d2167808c0bb8b37e
Shane McDonough <shanemcdman@gmail.com>
2020-08-11 14:51:41 -0700

    Create another constructor

--- include/ConwaysGame.h
+++ include/ConwaysGame.h
@@ -1,10 +1,14 @@
 #ifndef CONWAYS_GAME
 #define CONWAYS_GAME
 
+#include<raylib.h>
+
 class ConwaysGame{
 private:
+    Vector2 window_size;
 public:
     ConwaysGame();
+    ConwaysGame(float x, float y);
 };
 
 #endif //CONWAYS_GAME
--- src/ConwaysGame.cpp
+++ src/ConwaysGame.cpp
@@ -1,4 +1,9 @@
 #include "ConwaysGame.h"
 
-ConwaysGame::ConwaysGame(){
+ConwaysGame::ConwaysGame():ConwaysGame(600, 900){
+}
+
+ConwaysGame::ConwaysGame(float x, float y){
+    window_size.x = x;
+    window_size.y = y;
 }
