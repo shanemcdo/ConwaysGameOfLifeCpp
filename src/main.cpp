cea8c42e4eea5a3020efa6330a055c1c185d7745
Shane McDonough <shanemcdman@gmail.com>
2020-08-11 22:38:58 -0700

    Take one arg as scale

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
+#include<iostream>
 #include<ConwaysGame.h>
 
-int main(){
-    ConwaysGame game(1300, 660, 15, 30);
+int main(int argc, char** argv){
+    int scale = 10;
+    for(int i = 0; i < argc; i++){
+        std::cout << argv[i] << " ";
+    }
+    if(argc > 1){
+        scale = std::stoi(argv[1]);
+    }
+    ConwaysGame game(1300, 660, scale, 30);
     game.run();
 	return 0;
 }
