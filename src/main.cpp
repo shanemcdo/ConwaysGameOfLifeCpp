806c1f15be9c66490ed79e8e352764c09fa09bca
Shane McDonough <shanemcdman@gmail.com>
2020-08-13 18:23:27 -0700

    Remove log line

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,6 @@
 
 int main(int argc, char** argv){
     int scale = 10;
-    for(int i = 0; i < argc; i++){
-        std::cout << argv[i] << " ";
-    }
     if(argc > 1){
         scale = std::stoi(argv[1]);
     }
